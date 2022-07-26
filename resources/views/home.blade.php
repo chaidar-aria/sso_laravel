3b5ae6df555ddf10e69f7b9671b13459d6384a7f
Chaidar-Aria <67509854+Chaidar-Aria@users.noreply.github.com>
2022-07-26 22:14:21 +0700

    first commit app2

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        {{ __('You are logged in!') }}
+                    </div>
+                    <div class="card-body">
+                        <a href="http://app_sso1.main.test" class="text-sm text-gray-700 dark:text-gray-500 underline">SSO
+                            APP 1</a>
+                        <a href="http://app_sso3.main.test" class="text-sm text-gray-700 dark:text-gray-500 underline">SSO
+                            APP 3</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
